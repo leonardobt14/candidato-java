93c77ab18e3b5e359815672fa5f3be4aedd64af4
leonardobt14 <leonardo3ktt@gmail.com>
2020-05-13 19:42:16 -0300

    Leitura dos outros dados

--- src/Projeto.java
+++ src/Projeto.java
@@ -8,7 +8,8 @@ public class Projeto {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int escolaridade;
+		int escolaridade, experiencia;
+		char viajens, habilitacao;
 		
 		System.out.println ("Qual sua escolaridade?");
 		System.out.println ("1) Ensino Fundamental");
@@ -17,6 +18,17 @@ public class Projeto {
 		System.out.println ("4) Pós-graduação");
 		System.out.print ("Digite uma opção: ");
 		escolaridade = sc.nextInt();
+		
+		System.out.println();
+		System.out.print("Você tem quantos anos de experiência profissional? ");
+		experiencia = sc.nextInt();
+		System.out.print("Você tem disponibilidade para viajar (S/N)? ");
+		viajens = sc.next().charAt(0);
+		System.out.print("Você tem habilitação de motorista (S/N)? ");
+		habilitacao = sc.next().charAt(0);
+		
+		
+		sc.close();
 	}
 
 }
